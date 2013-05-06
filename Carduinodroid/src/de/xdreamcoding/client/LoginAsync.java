d8f60ebb6174f3d60b6bdc325758b0ba0c813cef
xDreamCoding <xdreamcoding@live.de>
2013-05-06 21:34:13 +0200

    new loginpage

---
+++ Carduinodroid/src/de/xdreamcoding/client/LoginAsync.java
@@ -0,0 +1,5 @@
+package de.xdreamcoding.client;
+
+public interface LoginAsync {
+
+}
---
+++ Carduinodroid/src/de/xdreamcoding/client/LoginEntryPoint.java
@@ -0,0 +1,13 @@
+package de.xdreamcoding.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class LoginEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
