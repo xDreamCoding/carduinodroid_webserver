2ce19a1cb6b8b4d3fe9fc3b8b36a700289e0544f
xDreamCoding <xdreamcoding@live.de>
2013-05-07 02:42:56 +0200

    trying to get it work

--- Carduinodroid/src/de/xdreamcoding/client/LoginEntryPoint.java
+++ Carduinodroid/src/de/xdreamcoding/client/LoginEntryPoint.java
@@ -1,13 +1,27 @@
 package de.xdreamcoding.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class LoginEntryPoint implements EntryPoint {
+		
+//	private final LoginAsync LoginService = Login.Util.getInstance();
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
-
+		Button sendButton = new Button("Login");
+		TextBox loginField = new TextBox();
+		PasswordTextBox passwordField = new PasswordTextBox();
+		Label errorLabel = new Label();
+		
+		RootPanel.get("loginFieldContainer").add(loginField);
+		RootPanel.get("passwordFieldContainer").add(passwordField);
+		RootPanel.get("sendButtonContainer").add(sendButton);
+		RootPanel.get("errorLabelContainer").add(errorLabel);
 	}
 
 }
