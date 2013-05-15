fca5027be2afaaf39a0f529447f778e6d6ac56db
xDreamCoding <xdreamcoding@live.de>
2013-05-15 21:48:29 +0200

    added connect to frontpage

--- Carduinodroid/src/de/xdreamcoding/client/LoginEntryPoint.java
+++ Carduinodroid/src/de/xdreamcoding/client/LoginEntryPoint.java
@@ -16,73 +16,103 @@ import com.google.gwt.user.client.ui.TextBox;
 import de.xdreamcoding.shared.User;
 
 public class LoginEntryPoint implements EntryPoint {
-		
+
 	private final LoginAsync LoginService = Login.Util.getInstance();
-	private Button sendButton;
-	private TextBox loginField;
+	private Button loginButton, connectButton;
+	private TextBox loginField, ipField;
 	private PasswordTextBox passwordField;
 	private Label errorLabel;
-	
+
 	private static User user;
-	
+
 	@Override
 	public void onModuleLoad() {
-		
-		sendButton = new Button("Login");
-		sendButton.addStyleName("submit");
+
+		loginButton = new Button("Login");
+		loginButton.addStyleName("submit");
 		loginField = new TextBox();
 		loginField.addStyleName("text");
 		passwordField = new PasswordTextBox();
 		passwordField.addStyleName("text");
+		connectButton = new Button("Login");
+		connectButton.addStyleName("submit");
+		ipField = new TextBox();
+		ipField.addStyleName("text");
 		errorLabel = new Label();
-		
+
 		RootPanel.get("loginFieldContainer").add(loginField);
 		RootPanel.get("passwordFieldContainer").add(passwordField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
+		RootPanel.get("sendButtonContainer").add(loginButton);
+		RootPanel.get("ipFieldContainer").add(ipField);
+		RootPanel.get("connectButtonContainer").add(connectButton);
 		RootPanel.get("errorLabelContainer").add(errorLabel);
 		
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendLoginToServer();
-			}
 
-			/**
-			 * Fired when the user types in the nameField.
-			 */
+		loginButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				sendLogin();
+			}	
+		});
+		KeyUpHandler keyup = new KeyUpHandler() {
 			public void onKeyUp(KeyUpEvent event) {
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendLoginToServer();
+					sendLogin();
 				}
 			}
-		}
+		};
+		passwordField.addKeyUpHandler(keyup);
+		loginField.addKeyUpHandler(keyup);
+
+		connectButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				sendIP();
+			}	
+		});
+		ipField.addKeyUpHandler(new KeyUpHandler() {
+			public void onKeyUp(KeyUpEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					sendIP();
+				}
+			}
+		});
 		
-			
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		passwordField.addKeyUpHandler(handler);
+		
 	}
 
-	void sendLoginToServer() {
+	void sendLogin() {
 		errorLabel.setText("");
 		LoginService.loginUser(loginField.getText(), passwordField.getText(),
-			new AsyncCallback<User>() {
-				public void onFailure(Throwable caught) {
-					errorLabel.setText(caught.getMessage());
-				}
-
-				@Override
-				public void onSuccess(User result) {
-					if(result != null) {
-						user = result;
-						errorLabel.setText("Hello " + result.getName() + " you sucessfully logged in!");
-					} else {
-						errorLabel.setText("Login invalid!");
+				new AsyncCallback<User>() {
+					public void onFailure(Throwable caught) {
+						errorLabel.setText(caught.getMessage());
 					}
-				}
-			});
-		}
-}
 
+					@Override
+					public void onSuccess(User result) {
+						if (result != null) {
+							user = result;
+							errorLabel.setText("Hello " + result.getName()
+									+ " you sucessfully logged in!");
+						} else {
+							errorLabel.setText("Login invalid!");
+						}
+					}
+				});
+	}
+
+	void sendIP() {
+		errorLabel.setText("");
+		LoginService.connectIP(ipField.getText(),
+				new AsyncCallback<String>() {
+			public void onFailure(Throwable caught) {
+				// Show the RPC error message to the user
+				errorLabel.setText(caught.getMessage());
+
+			}
+
+			public void onSuccess(String result) {
+				errorLabel.setText(result);
+			}
+		});
+	}
+}
--- Carduinodroid/src/de/xdreamcoding/server/LoginImpl.java
+++ Carduinodroid/src/de/xdreamcoding/server/LoginImpl.java
@@ -14,6 +14,7 @@
  *******************************************************************************/
 package de.xdreamcoding.server;
 
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -23,34 +24,80 @@ import java.sql.Statement;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.xdreamcoding.client.Login;
+import de.xdreamcoding.desktop.Controller.Controller_Computer;
+import de.xdreamcoding.desktop.Model.Log;
+import de.xdreamcoding.shared.FieldVerifier;
 import de.xdreamcoding.shared.User;
 
 public class LoginImpl extends RemoteServiceServlet implements Login {
 
 	@Override
-	public String connectIP(String ip) throws IllegalArgumentException {
-		return null;
-	}
-
-	@Override
 	public User loginUser(String login, String password)  throws IllegalArgumentException {
+		
+		Log log = (Log) getServletContext().getAttribute("log");
 		
 		User user = new User();
 		Connection connection = (Connection) getServletContext().getAttribute("connection");
+		
 		try {
 			Statement stmt = connection.createStatement();
-			if(stmt.execute("SELECT name FROM user WHERE login = '" + login + "' AND password = '" + password + "';")) {
-				ResultSet rset = stmt.getResultSet();
+			stmt.execute("SELECT name FROM user WHERE login = '" + escapeHtml(login) + "' AND password = '" + escapeHtml(password) + "';");
+			ResultSet rset = stmt.getResultSet();
+			if(!rset.isBeforeFirst()) {
+				throw new IllegalArgumentException("Login invalid.");
+			} else {
 				rset.next();
 				user.setName(rset.getString(1));
-			} else {
-				stmt.close();
-				throw new IllegalArgumentException("Login invalid.");
 			}
 			stmt.close();
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
+			throw new IllegalArgumentException("Database Error.");
 		}
 		return user;
+	}
+	
+	@Override
+	public String connectIP(String input) throws IllegalArgumentException {
+		//Get Objects through ServletContext.
+		Log log = (Log) getServletContext().getAttribute("log");
+		Controller_Computer controller = (Controller_Computer) getServletContext().getAttribute("controller");
+		
+		// Verify that the input is valid. 
+		if (!FieldVerifier.isValidIP(input)) {
+			// If the input is not valid, throw an IllegalArgumentException back to
+			// the client.
+			throw new IllegalArgumentException("Please enter a valid IP.");
+		}
+
+
+		// Escape data from the client to avoid cross-site script vulnerabilities.
+		input = escapeHtml(input);
+		log.writelogfile("IP received " + input + " trying to connect.");
+		String stringReturn = "IP was " + input + ". ";
+		
+		boolean error = false;
+		try {
+			controller.network.connect(input);
+		} catch (UnknownHostException e) {
+			error = true;
+			e.printStackTrace();
+		}
+		if(error) {
+			log.writelogfile("Connection failure.");
+			throw new IllegalArgumentException(stringReturn + "Connection failure.");
+		} else {
+			log.writelogfile("Connection established.");
+			stringReturn += "Connection seems to be established.";
+		}
+		return stringReturn;
+	}
+	
+	private String escapeHtml(String html) {
+		if (html == null) {
+			return null;
+		}
+		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
+				.replaceAll(">", "&gt;");
 	}
 }
